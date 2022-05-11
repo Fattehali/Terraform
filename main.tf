a9f0ff8a673b14e5a7c7f26208b12ebb2d4c5ccf
Fattehali <88650927+Fattehali@users.noreply.github.com>
2022-05-11 16:08:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  alias  = "south"
+  region = "ap-south-1"
+  access_key = "AKIA2EKXCST4AHTJ2X2N"
+  secret_key = "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"
+}
+
 module "ec2" {
     source = "../ec2_module"
 }
