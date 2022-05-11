c59b507af46ff21548217e009856d7f379b0aefd
Fattehali <88650927+Fattehali@users.noreply.github.com>
2022-05-11 16:27:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
 provider "aws" {
   alias  = "south"
   region = "ap-south-1"
